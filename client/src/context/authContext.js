50927236a3d31d6bdef3f335560a650d9e7376dc
ahao <ahao20010222@gmail.com>
2024-08-15 21:25:29 +0800

    添加文档和图片文件夹

--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -10,7 +10,7 @@ export const AuthContextProvider = ({ children }) => {
 
   const login = async (inputs) => {
     const res = await axios.post(
-      "http://localhost:8800/api/auth/login",
+      "http://localhost:8080/api/auth/login",
       inputs,
       {
         withCredentials: true,
